c6938b0ee706f5f7f04b5016a30196a29511ff86
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2022-12-05 01:24:31 +0000

    [Update]3-TASK2

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -3,18 +3,5 @@ Rails.application.routes.draw do
   root to:  'homes#top'
 # ####################################################################
   resources :books
-    ####################################################################
-     # get 'books'.    => 'books#index',as: 'index'
-     # post 'books'    => 'books#create',as: 'create'
-    ####################################################################
-     # get 'books/new' => 'books#new',as: 'new'
-    ####################################################################
-     # get 'books/:id' => 'books#show',as: 'show'
-    ####################################################################
-     # get 'books/:id/edit' => 'books#edit',as: 'edit'
-     # patch 'books/:id' => 'books#update',as: 'update'
-    ####################################################################
-     # delete 'books/:id' => 'books#destroy',as: 'destroy'
-    ####################################################################
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
